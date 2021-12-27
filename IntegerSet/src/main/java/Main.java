b041fa688736e37e0e9095bf34adb54b546b8196
Melanie Orellana <melanievane96@gmail.com>
2021-12-27 14:33:47 -0600

    Added IntegerSet implementation

--- IntegerSet/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IntegerSet set = new IntegerSet();
-        set.insert(1);
-        set.remove(1);
-
-        System.out.println(set);
-    }
-}
